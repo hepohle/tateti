0f3f8614377a0dfcbea9ab8a7a279f833a678ab9
Hector <hectorpohle@gmail.com>
2021-11-13 11:24:31 -0300

    cambio nombre archivo apellidos

---
+++ programaOrozcoPohle.php
@@ -0,0 +1,68 @@
+<?php
+include_once("tateti.php");
+
+/**************************************/
+/***** DATOS DE LOS INTEGRANTES *******/
+/**************************************/
+
+/* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
+/* ... COMPLETAR ... */
+
+// Orozco Iván
+
+// Pohle Héctor FAI 3715
+
+// https://github.com/hepohle/tateti
+
+/**************************************/
+/***** DEFINICION DE FUNCIONES ********/
+/**************************************/
+
+
+
+
+
+
+
+
+/**************************************/
+/*********** PROGRAMA PRINCIPAL *******/
+/**************************************/
+
+//Declaración de variables:
+
+
+//Inicialización de variables:
+
+
+//Proceso:
+
+$juego = jugar();
+//print_r($juego);
+//imprimirResultado($juego);
+
+
+
+/*
+do {
+    $opcion = ...;
+
+    
+    switch ($opcion) {
+        case 1: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 1
+
+            break;
+        case 2: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 2
+
+            break;
+        case 3: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
+
+            break;
+        
+            //...
+    }
+} while ($opcion != X);
+*/
